60ff3380121dfc3f12902b60e73e0962656a60e0
momo13910 <mohamed.yahyaoui10@sfr.fr>
2019-09-08 15:46:17 +0200

    Version 1.60

--- js/accueil.js
+++ js/accueil.js
@@ -1,1 +1,125 @@
 
+let image = $('.pizzajesus');
+let napolitaine = $('.napo');
+let orient = $('.orient');
+let savoyarde = $('.savoya');
+let vege = $('.vegeta');
+let reine = $('.reine');
+let quatreFro = $('.quatreFro');
+let mer = $('.mer');
+let margua = $('.margua');
+
+function jesus(num) {
+    switch (num) {
+        case 0:
+                margua.css('background', '');
+                mer.css('background', '');
+            quatreFro.css('background', '');
+            reine.css('background', '');
+            vege.css('background', '');
+            savoyarde.css('background', '');
+            image.css('background', '');
+            napolitaine.css('background', '');
+            orient.css('background', '');
+            break;
+        case 1:
+                margua.css('background', '');
+                mer.css('background', '');
+            quatreFro.css('background', '');
+            reine.css('background', '');
+            vege.css('background', '');
+            savoyarde.css('background', '');
+            orient.css('background', '');
+            napolitaine.css('background', '');
+            image.css('background', 'url(https://hdqwalls.com/download/pizza-baked-chesse-spicy-7t-1920x1080.jpg) no-repeat');
+            break;
+        case 2:
+                margua.css('background', '');
+                mer.css('background', '');
+            quatreFro.css('background', '');
+            reine.css('background', '');
+            vege.css('background', '');
+            savoyarde.css('background', '');
+            orient.css('background', '');
+            image.css('background', '');
+            napolitaine.css('background', 'url(http://www.voyage.fr/sites/default/files/styles/1170x720/public/2018-10/00-napolitaine-romaine-quelle-est-vraiment-l-origine-de-la-pizza.jpg?itok=q35L_NY5) no-repeat');
+            break;
+        case 3:
+                margua.css('background', '');
+                mer.css('background', '');
+            quatreFro.css('background', '');
+            reine.css('background', '');
+            vege.css('background', '');
+            savoyarde.css('background', '');
+            image.css('background', '');
+            napolitaine.css('background', '');
+            orient.css('background', 'url(http://www.pizzeria-chez-marie.fr/wp-content/uploads/2018/09/Merguez.jpg) no-repeat')
+            break;
+        case 4:
+                margua.css('background', '');
+                mer.css('background', '');
+            quatreFro.css('background', '');
+            reine.css('background', '');
+            vege.css('background', '');
+            image.css('background', '');
+            napolitaine.css('background', '');
+            orient.css('background', '');
+            savoyarde.css('background', 'url(http://miam-pizza.com/images/tartiflette.jpeg) no-repeat')
+            break;
+        case 5:
+                margua.css('background', '');
+                mer.css('background', '');
+            quatreFro.css('background', '');
+            reine.css('background', '');
+            image.css('background', '');
+            napolitaine.css('background', '');
+            orient.css('background', '');
+            savoyarde.css('background', '');
+            vege.css('background', 'url(https://www.femininbio.com/sites/femininbio.com/files/styles/paysage/public/styles/paysage/public/images/2015/08/pizza_vegetarienne_aux_legumes_confits_04_-_bd_c_marie_chioca.jpg?itok=_g4-2Swj) no-repeat')
+            break;
+        case 6:
+                margua.css('background', '');
+                mer.css('background', '');
+            quatreFro.css('background', '');
+            reine.css('background', 'url(https://www.pizza-delea.com/wp-content/uploads/2017/10/REINE.jpg) no-repeat');
+            image.css('background', '');
+            napolitaine.css('background', '');
+            orient.css('background', '');
+            savoyarde.css('background', '');
+            vege.css('background', '')
+            break;
+        case 7:
+                margua.css('background', '');
+                mer.css('background', '');
+            quatreFro.css('background', 'url(https://le80.fr/wp-content/uploads/2017/05/pizza-4-fromages.jpg) no-repeat');
+            reine.css('background', '');
+            image.css('background', '');
+            napolitaine.css('background', '');
+            orient.css('background', '');
+            savoyarde.css('background', '');
+            vege.css('background', '')
+            break;
+        case 8:
+                margua.css('background', '');
+            mer.css('background', 'url(https://assets.afcdn.com/recipe/20160928/17857_w1024h768c1cx2596cy1733.jpg) no-repeat');
+            quatreFro.css('background', '');
+            reine.css('background', '');
+            image.css('background', '');
+            napolitaine.css('background', '');
+            orient.css('background', '');
+            savoyarde.css('background', '');
+            vege.css('background', '')
+            break;
+        case 9:
+            margua.css('background', 'url(http://images.smuckers.ca/images/recipes/29/fr/Pizza-margarita-sans-gluten_desktop.jpg) no-repeat');
+                mer.css('background', '');
+            quatreFro.css('background', '');
+            reine.css('background', '');
+            image.css('background', '');
+            napolitaine.css('background', '');
+            orient.css('background', '');
+            savoyarde.css('background', '');
+            vege.css('background', '')
+            break;
+    }
+}
