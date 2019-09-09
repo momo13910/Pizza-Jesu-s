b6ddc1827554a604276819941b911d2f607da208
momo13910 <mohamed.yahyaoui10@sfr.fr>
2019-09-09 11:43:24 +0200

    Version 1.8

---
+++ js/contact.js
@@ -0,0 +1,34 @@
+let name = $('#name');
+let email = $('#email');
+let message = $('#message');
+let valider = $('#valider');
+
+
+valider.click(() => {
+
+    $.ajax({
+        url: 'localhost/Pizza-Jesu-s/php/mail.php',
+        type: 'POST',
+        data: {
+
+            name: name.val(),
+            email: email.val(),
+            messag: message.val()
+
+
+        },
+        success: 
+        function marche(result) {
+
+            alert(result);
+        },
+        error:
+         function error(erreur) {
+
+            console.log(erreur);
+        }
+
+    })
+
+});
+
